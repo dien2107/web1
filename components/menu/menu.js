5052fe9cabcf930e24b39e0d30f6f8659708bfdb
dien2107 <nguyenthanhdien910@gmail.com>
2024-10-12 11:44:07 +0700

    menu

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -3,13 +3,11 @@ const closeMenuBtn = document.querySelector('.hide__menu--close');
 const hideMenu = document.querySelector('.hide__menu');
 const overlay = document.querySelector('.overlay');
 
-//Open hide menu
 hideMenuBtn.addEventListener('click', e => {
   hideMenu.classList.toggle('active');
   overlay.classList.toggle('active__overlay');
 });
 
-//Close menu function
 const closeMenu = () => {
   hideMenu.classList.remove('active');
   overlay.classList.remove('active__overlay');
@@ -23,20 +21,13 @@ overlay.addEventListener('click', e => {
   closeMenu();
 });
 
-//Account click event
 const userBtn = document.querySelector('.hide__menu--list__extention .header__bottom--extention-user');
 const userWrapper = document.querySelector('.user__wrapper');
-// userBtn.addEventListener('click', (e) => {
-//   hideMenu.classList.remove('active');
-// })
-
 const openFormRegister = () => {
   userWrapper.classList.add('user__active');
   userWrapper.classList.add('register__active');
   overlay.classList.add('active__overlay');
 };
-
-//Show more catelogy
 const catelogyBtn = document.querySelector('.hide__menu--list__extention .header__bottom--extention-cate');
 const hideMenuList = document.querySelector('.hide__menu--list__types');
 
